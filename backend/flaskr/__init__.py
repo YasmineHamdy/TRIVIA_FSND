815cf99517665c65c2d0f586b438b300bef98518
Yasmine <yhamdy@iotblue.net>
2020-08-09 02:56:40 +0200

    add 405, 500 error handler

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -58,6 +58,24 @@ def create_app(test_config=None):
       "message": "bad request"
       }), 400
 
+  
+  @app.errorhandler(405)
+  def not_allowed(error):
+    return jsonify({
+      "success": False, 
+      "error": 405,
+      "message": "method not allowed"
+      }), 405
+
+  @app.errorhandler(500)
+  def internal_error(error):
+    return jsonify({
+      "success": False, 
+      "error": 500,
+      "message": "internal server error"
+      }), 500
+
+
   return app
 
     
